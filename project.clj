ef236661a7eb6e1800e360d254fdfbd380c711ba
Sarah Roberts <sonoran.sarah@gmail.com>
2025-04-11 17:48:56 -0700

    added the license to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject org.cyverse/clj-kondo-exports "0.1.1-SNAPSHOT"
   :description "clj-kondo configuration and macro expansons for third-party libraries"
+  :license {:name "BSD"
+            :url "https://cyverse.org/license"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [korma "0.4.3"]]
   :plugins [[com.github.clj-kondo/lein-clj-kondo "2025.02.20"]]
